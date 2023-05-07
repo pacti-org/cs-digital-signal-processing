99adadd1e15cf099fe17212f0dafa90342362383
Nicolas Rouquette <nfr@jpl.nasa.gov>
2023-05-07 11:53:59 -0700

    updated statistics

--- pacti_counters.py
+++ pacti_counters.py
@@ -2,6 +2,7 @@ from functools import wraps
 from typing import Dict, List, Tuple
 from pacti.terms.polyhedra import PolyhedralContract
 
+import sys
 import functools
 import dataclasses
 
@@ -15,8 +16,11 @@ class PolyhedralContractSize:
     # The count of input and output variables.
     variables: int = 0
 
-    def __init__(self, contract: PolyhedralContract | None):
-        if contract:
+    def __init__(self, contract: PolyhedralContract | None, max_values: bool = False):
+        if max_values:
+            self.constraints = sys.maxsize
+            self.variables = sys.maxsize
+        elif contract:
             self.constraints = len(contract.a.terms) + len(contract.g.terms)
             self.variables = len(contract.vars)
         else:
@@ -34,9 +38,15 @@ class PolyhedralContractSize:
             return self
         else:
             return other
-        
+
+    @staticmethod
+    def _render_(value: int) -> str:
+        if sys.maxsize == value:
+            return "inf"
+        return str(value)
+
     def __str__(self) -> str:
-        return f"(constraints: {self.constraints}, variables: {self.variables})"
+        return f"(constraints: {PolyhedralContractSize._render_(self.constraints)}, variables: {PolyhedralContractSize._render_(self.variables)})"
 
 
 def statistics_decorator(fn):
@@ -52,14 +62,16 @@ def statistics_decorator(fn):
         input_size2 = PolyhedralContractSize(contract=args[1])
         result_size = PolyhedralContractSize(contract=result_contract)
 
-        # Update the maximum polyhedral contract size if necessary
+        # Update the min/max polyhedral contract size if necessary
+        wrapper.min_size = min(wrapper.min_size, input_size1, input_size2, result_size)
         wrapper.max_size = max(wrapper.max_size, input_size1, input_size2, result_size)
 
         # Return the composed contract
         return result_contract
 
     wrapper.counter = 0
-    wrapper.max_size = PolyhedralContractSize(contract=None)
+    wrapper.min_size = PolyhedralContractSize(contract=None, max_values=True)
+    wrapper.max_size = PolyhedralContractSize(contract=None, max_values=False)
     return wrapper
 
 
@@ -74,14 +86,34 @@ class PolyhedralContractCounts:
     quotient_count: int = 0
     merge_count: int = 0
 
-    compose_max_size: PolyhedralContractSize = dataclasses.field(default_factory=lambda: PolyhedralContractSize(contract=None))
-    quotient_max_size: PolyhedralContractSize = dataclasses.field(default_factory=lambda: PolyhedralContractSize(contract=None))
-    merge_max_size: PolyhedralContractSize = dataclasses.field(default_factory=lambda: PolyhedralContractSize(contract=None))
+    compose_min_size: PolyhedralContractSize = dataclasses.field(
+        default_factory=lambda: PolyhedralContractSize(contract=None, max_values=True)
+    )
+    quotient_min_size: PolyhedralContractSize = dataclasses.field(
+        default_factory=lambda: PolyhedralContractSize(contract=None, max_values=True)
+    )
+    merge_min_size: PolyhedralContractSize = dataclasses.field(
+        default_factory=lambda: PolyhedralContractSize(contract=None, max_values=True)
+    )
+
+    compose_max_size: PolyhedralContractSize = dataclasses.field(
+        default_factory=lambda: PolyhedralContractSize(contract=None, max_values=False)
+    )
+    quotient_max_size: PolyhedralContractSize = dataclasses.field(
+        default_factory=lambda: PolyhedralContractSize(contract=None, max_values=False)
+    )
+    merge_max_size: PolyhedralContractSize = dataclasses.field(
+        default_factory=lambda: PolyhedralContractSize(contract=None, max_values=False)
+    )
 
     def update_counts(self) -> "PolyhedralContractCounts":
         self.compose_count = PolyhedralContract.compose.counter
         self.quotient_count = PolyhedralContract.quotient.counter
         self.merge_count = PolyhedralContract.merge.counter
+
+        self.compose_min_size = PolyhedralContract.compose.min_size
+        self.quotient_min_size = PolyhedralContract.quotient.min_size
+        self.merge_min_size = PolyhedralContract.merge.min_size
 
         self.compose_max_size = PolyhedralContract.compose.max_size
         self.quotient_max_size = PolyhedralContract.quotient.max_size
@@ -95,6 +127,10 @@ class PolyhedralContractCounts:
         result.quotient_count = self.quotient_count + other.quotient_count
         result.merge_count = self.merge_count + other.merge_count
 
+        result.compose_min_size = min(self.compose_min_size, other.compose_min_size)
+        result.quotient_min_size = min(self.quotient_min_size, other.quotient_min_size)
+        result.merge_min_size = min(self.merge_min_size, other.merge_min_size)
+
         result.compose_max_size = max(self.compose_max_size, other.compose_max_size)
         result.quotient_max_size = max(self.quotient_max_size, other.quotient_max_size)
         result.merge_max_size = max(self.merge_max_size, other.merge_max_size)
@@ -104,9 +140,9 @@ class PolyhedralContractCounts:
     def __str__(self) -> str:
         return (
             f"PolyhedralContract operation counts: compose={self.compose}, quotient={self.quotient}, merge={self.merge}.\n"
-            + f"max sizes for compose={self.compose_max_size}\n"
-            + f"max sizes for quotient={self.quotient_max_size}\n"
-            + f"max sizes for merge={self.merge_max_size}\n"
+            + f"min/max sizes for compose={self.compose_min_size}/{self.compose_max_size}\n"
+            + f"min/max sizes for quotient={self.quotient_min_size}/{self.quotient_max_size}\n"
+            + f"min/max sizes for merge={self.merge_min_size}/{self.merge_max_size}\n"
         )
 
 
@@ -124,18 +160,35 @@ class PolyhedralContractCountStats:
     avg_quotient: float = 0
     avg_merge: float = 0
 
-    compose_max_size: PolyhedralContractSize = dataclasses.field(default_factory=lambda: PolyhedralContractSize(contract=None))
-    quotient_max_size: PolyhedralContractSize = dataclasses.field(default_factory=lambda: PolyhedralContractSize(contract=None))
-    merge_max_size: PolyhedralContractSize = dataclasses.field(default_factory=lambda: PolyhedralContractSize(contract=None))
+    compose_min_size: PolyhedralContractSize = dataclasses.field(
+        default_factory=lambda: PolyhedralContractSize(contract=None, max_values=True)
+    )
+    quotient_min_size: PolyhedralContractSize = dataclasses.field(
+        default_factory=lambda: PolyhedralContractSize(contract=None, max_values=True)
+    )
+    merge_min_size: PolyhedralContractSize = dataclasses.field(
+        default_factory=lambda: PolyhedralContractSize(contract=None, max_values=True)
+    )
+
+    compose_max_size: PolyhedralContractSize = dataclasses.field(
+        default_factory=lambda: PolyhedralContractSize(contract=None, max_values=False)
+    )
+    quotient_max_size: PolyhedralContractSize = dataclasses.field(
+        default_factory=lambda: PolyhedralContractSize(contract=None, max_values=False)
+    )
+    merge_max_size: PolyhedralContractSize = dataclasses.field(
+        default_factory=lambda: PolyhedralContractSize(contract=None, max_values=False)
+    )
 
     def stats(self) -> str:
-        return ("Pacti compose,quotient,merge statistics:\n"
+        return (
+            "Pacti compose,quotient,merge statistics:\n"
             + f"min: (compose: {self.min_compose}, quotient: {self.min_quotient}, merge: {self.min_merge})\n"
             + f"max: (compose: {self.max_compose}, quotient: {self.max_quotient}, merge: {self.max_merge})\n"
             + f"avg: (compose: {self.avg_compose}, quotient: {self.avg_quotient}, merge: {self.avg_merge})\n"
-            + f"max compose size: {self.compose_max_size}\n"
-            + f"max quotient size: {self.quotient_max_size}\n"
-            + f"max merge size: {self.merge_max_size}\n"
+            + f"min/max compose size: {self.compose_min_size}/{self.compose_max_size}\n"
+            + f"min/max quotient size: {self.quotient_min_size}/{self.quotient_max_size}\n"
+            + f"min/max merge size: {self.merge_min_size}/{self.merge_max_size}\n"
         )
 
 
@@ -156,6 +209,11 @@ def polyhedral_count_stats(counts: List[PolyhedralContractCounts]) -> Polyhedral
     stats.avg_compose: float = sum(count.compose_count for count in counts) / n
     stats.avg_quotient: float = sum(count.quotient_count for count in counts) / n
     stats.avg_merge: float = sum(count.merge_count for count in counts) / n
+
+    # Minimum PolyhedralContractSize for each operation
+    stats.compose_min_size = min(count.compose_min_size for count in counts)
+    stats.quotient_min_size = min(count.quotient_min_size for count in counts)
+    stats.merge_min_size = min(count.merge_min_size for count in counts)
 
     # Maximum PolyhedralContractSize for each operation
     stats.compose_max_size = max(count.compose_max_size for count in counts)
